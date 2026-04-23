353f03d773a2a6977a5a887585b447da6fd40714
Alan-aggregatum <agarcia@aggregatum.com>
2026-04-22 19:36:26 -0600

    Feature: Mejora de diseño

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 function filterSelection(category) {
   let items = document.getElementsByClassName("item");
+  let buttons = document.querySelectorAll(".filters button");
+
+  // Quitar clase active de todos los botones
+  buttons.forEach(btn => btn.classList.remove("active"));
+  // Agregar al botón clickeado (esto es opcional pero se ve pro)
+  event.target.classList.add("active");
 
   if (category === "all") category = "";
 
   for (let i = 0; i < items.length; i++) {
     items[i].style.display = "none";
-
     if (items[i].className.indexOf(category) > -1) {
       items[i].style.display = "block";
     }
